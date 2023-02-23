ccc75a4dca7dc837cc492def43d1e1a416a4022e
NJ9802 <nelsonjavier9802@gmail.com>
2023-02-23 10:10:03 -0500

    Reestructure the App and split in more components.

--- frontend/chatapp-frontend/src/scene/home/index.jsx
+++ frontend/chatapp-frontend/src/scene/home/index.jsx
@@ -1,12 +1,43 @@
 import Navbar from "../../components/Navbar";
 import { Box } from "@mui/material";
 import ConversationIndex from "../../components/ConversationIndex";
+import {
+  MainContainer,
+  Sidebar,
+  ChatContainer,
+} from "@chatscope/chat-ui-kit-react";
+import ChatRoom from "../../components/ChatRoom";
+import { useState } from "react";
+import ConversationListSideBar from "../../components/ConversationListSideBar";
 
 const Home = () => {
+  // Set initial message input value to an empty string
+  const [messageInputValue, setMessageInputValue] = useState("");
+
   return (
     <Box>
-      <Navbar />
-      <ConversationIndex />
+      <Box display={{ sm: "none" }}>
+        <Navbar />
+      </Box>
+      <Box
+        sx={{
+          display: { xs: "none", sm: "block" },
+          height: { xs: "calc(100vh - 114px)", sm: "100vh" },
+          position: "relative",
+        }}
+      >
+        <MainContainer responsive>
+          <div as={Sidebar}>
+            <ConversationListSideBar />
+          </div>
+          <div as={ChatContainer} style={{ display: "flex", flexGrow: 1 }}>
+            <ChatRoom />
+          </div>
+        </MainContainer>
+      </Box>
+      <Box display={{ sm: "none" }}>
+        <ConversationIndex />
+      </Box>
     </Box>
   );
 };
